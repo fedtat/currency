17ff603566b5f9b5c497e6f4877a3f2c3ad00207
fedtat <fedorova.int@gmail.com>
2022-03-20 15:42:30 +0200

    added rates db and html templates for table view of rates and contacts

--- app/currency/models.py
+++ app/currency/models.py
@@ -5,3 +5,11 @@ class ContactUs(models.Model):
     email_from = models.EmailField(max_length=254)
     subject = models.CharField(max_length=64)
     message = models.CharField(max_length=1000)
+
+
+class Rate(models.Model):
+    type = models.CharField(max_length=5)  # noqa: A003
+    source = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+    buy = models.DecimalField(max_digits=10, decimal_places=2)
+    sell = models.DecimalField(max_digits=10, decimal_places=2)
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -6,6 +6,8 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', currency_views.index),
     path('hello-world/', currency_views.hello_world),
-    path('contacts/list', currency_views.contacts_list),
+    path('contacts/list/', currency_views.contacts_list),
+    path('rate/list/', currency_views.rate_list),
 ]
