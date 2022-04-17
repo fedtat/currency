913f3ea450c629184802d5969ed63665100991c8
fedtat <fedorova.int@gmail.com>
2022-04-17 17:14:22 +0300

    added sign up form

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -7,6 +7,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('my-profile/', accounts_views.MyProfile.as_view(), name='my-profile'),
+    path('signup/', accounts_views.SignUp.as_view(), name='signup'),
+    path('activate/<uuid:username>/', accounts_views.ActivateUser.as_view(), name='activate-user'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('change_password/', auth_views.PasswordChangeView.as_view(), name='change_password'),
 ]
