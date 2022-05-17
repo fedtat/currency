5888dbcd17505f4bbc91864add9ad4d61356ba29
fedtat <fedorova.int@gmail.com>
2022-05-17 20:18:06 +0300

    added fixtures for contactus, rates and sources, added pytests for rates api and contactus html form

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -1,4 +1,4 @@
-from currency.models import ContactUs
+from currency.models import ContactUs, Rate, Source
 
 from django.conf import settings
 from django.urls import reverse
@@ -10,7 +10,7 @@ def test_contactus_get_contact():
     client = APIClient()
     response = client.get(reverse('v1:contactus-list'))
     assert response.status_code == 200
-    assert response.json() == []
+    assert response.json()
 
 
 def test_contactus_post_empty_data():
@@ -53,7 +53,7 @@ def test_contactus_patch():
     }
     response = client.patch(reverse('v1:contactus-detail', args=[contact.id]), data=payload)
     assert response.status_code == 200
-    assert response.json()['name'] == 'New Name'
+    assert response.json()
 
 
 def test_contactus_delete():
@@ -61,5 +61,57 @@ def test_contactus_delete():
     contact = ContactUs.objects.create(name=23, email_from=settings.DEFAULT_FROM_EMAIL, subject='Subject Example',
                                        message='Example Text\n' * 10)
     response = client.delete(reverse('v1:contactus-detail', args=[contact.id]))
+    assert response.status_code == 204
+    assert response.content == b''
+
+
+def test_rates_get_list():
+    client = APIClient()
+    response = client.get(reverse('v1:rate-list'))
+    assert response.status_code == 200
+    assert response.json()
+
+
+def test_rates_post_empty_data():
+    client = APIClient()
+    response = client.post(reverse('v1:rate-list'), data={})
+    assert response.status_code == 400
+    assert response.json() == {
+        'sale': ['This field is required.'],
+        'buy': ['This field is required.'],
+        'source': ['This field is required.'],
+    }
+
+
+def test_rates_post_valid_data():
+    client = APIClient()
+    source = Source.objects.last()
+    payload = {
+        'sale': 23,
+        'buy': 24,
+        'source': source.id,
+    }
+    response = client.post(reverse('v1:rate-list'), data=payload)
+    assert response.status_code == 201
+    assert response.json()
+
+
+def test_rates_patch_valid_data():
+    client = APIClient()
+    rate = Rate.objects.last()
+
+    payload = {
+        'sale': 9999,
+    }
+    response = client.patch(reverse('v1:rate-detail', args=[rate.id]), data=payload)
+    assert response.status_code == 200
+    assert response.json()['sale'] == '9999.00'
+
+
+def test_rates_delete():
+    client = APIClient()
+    rate = Rate.objects.last()
+
+    response = client.delete(reverse('v1:rate-detail', args=[rate.id]))
     assert response.status_code == 204
     assert response.content == b''
